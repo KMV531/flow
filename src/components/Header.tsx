314c03f953067ef2f74dfc7b02a7e2cb64b0adbd
KMV531 <koladjamomovinnybrayan3@gmail.com>
2025-03-26 16:16:56 +0100

    Header completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+import MobileNav from './MobileNav'
+
+const NavLinks = [
+  {
+    title: 'About Us',
+    href: '/about-us',
+  },
+  {
+    title: 'Blog',
+    href: '/blog',
+  },
+  {
+    title: 'Careers',
+    href: '/careers',
+  },
+  {
+    title: 'Pricing',
+    href: '/pricing',
+  },
+]
+
+const Header = () => {
+  return (
+    <header className='container mx-auto'>
+      <div className='flex items-center justify-between gap-5 py-7 px-7 lg:px-0'>
+        <div className='flex items-center justify-center gap-10'>
+          <Link href={'/'} className='flex items-center gap-2'>
+            <Image
+              src={'/logo.png'}
+              alt='logo'
+              width={25}
+              height={25}
+              className='object-cover'
+            />
+            <h1 className='font-semibold text-2xl'>FLOW</h1>
+          </Link>
+          <div className='hidden md:flex items-center gap-5'>
+            {NavLinks.map((NavLink, index) => {
+              return (
+                <Link href={NavLink.href} key={index} className='gap-x-10'>
+                  {NavLink.title}
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+        <div className='flex items-center gap-4'>
+          <div className='block md:hidden'>
+            <MobileNav />
+          </div>
+          <Button className='border-2 border-white p-5 rounded-full hover:bg-[#BFF265] hover:text-black cursor-pointer'>
+            Get in touch
+          </Button>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { Menu, X } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const NavLinks = [
+  {
+    title: 'About Us',
+    href: '/about-us',
+  },
+  {
+    title: 'Blog',
+    href: '/blog',
+  },
+  {
+    title: 'Careers',
+    href: '/careers',
+  },
+  {
+    title: 'Pricing',
+    href: '/pricing',
+  },
+]
+
+const SHEET_SIDES = ['top'] as const
+
+type MobileNav = (typeof SHEET_SIDES)[number]
+
+function MobileNav() {
+  return (
+    <div>
+      {SHEET_SIDES.map((side) => (
+        <Sheet key={side}>
+          <SheetTrigger asChild>
+            <Menu className='cursor-pointer' />
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>
+                <div className='flex justify-between px-3 py-3'>
+                  <Link href={'/'} className='flex items-center gap-2'>
+                    <Image
+                      src={'/logo.png'}
+                      alt='logo'
+                      width={25}
+                      height={25}
+                      className='object-cover'
+                    />
+                    <h1 className='font-semibold text-2xl'>FLOW</h1>
+                  </Link>
+                  <Button className='border-2 border-white p-5 rounded-full hover:bg-[#BFF265] hover:text-black cursor-pointer'>
+                    Get in touch
+                  </Button>
+                </div>
+              </SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+            <div className='flex flex-col justify-center items-center gap-10 pt-10 '>
+              {NavLinks.map((NavLink, index) => {
+                return (
+                  <Link href={NavLink.href} key={index} className='gap-x-10'>
+                    {NavLink.title}
+                  </Link>
+                )
+              })}
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button className='absolute top-9 right-[10rem] cursor-pointer'>
+                  <X className='size-7' />
+                </Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      ))}
+    </div>
+  )
+}
+
+export default MobileNav
