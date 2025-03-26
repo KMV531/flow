8a8c7c459b487cc72f0a3fab9c9536286eefe7bb
KMV531 <koladjamomovinnybrayan3@gmail.com>
2025-03-26 16:36:09 +0100

    Header line added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,39 +25,42 @@ const NavLinks = [
 
 const Header = () => {
   return (
-    <header className='container mx-auto'>
-      <div className='flex items-center justify-between gap-5 py-7 px-7 lg:px-0'>
-        <div className='flex items-center justify-center gap-10'>
-          <Link href={'/'} className='flex items-center gap-2'>
-            <Image
-              src={'/logo.png'}
-              alt='logo'
-              width={25}
-              height={25}
-              className='object-cover'
-            />
-            <h1 className='font-semibold text-2xl'>FLOW</h1>
-          </Link>
-          <div className='hidden md:flex items-center gap-5'>
-            {NavLinks.map((NavLink, index) => {
-              return (
-                <Link href={NavLink.href} key={index} className='gap-x-10'>
-                  {NavLink.title}
-                </Link>
-              )
-            })}
+    <>
+      <header className='container mx-auto'>
+        <div className='flex items-center justify-between gap-5 py-7 px-7 lg:px-0'>
+          <div className='flex items-center justify-center gap-10'>
+            <Link href={'/'} className='flex items-center gap-2'>
+              <Image
+                src={'/logo.png'}
+                alt='logo'
+                width={25}
+                height={25}
+                className='object-cover'
+              />
+              <h1 className='font-semibold text-2xl'>FLOW</h1>
+            </Link>
+            <div className='hidden md:flex items-center gap-5'>
+              {NavLinks.map((NavLink, index) => {
+                return (
+                  <Link href={NavLink.href} key={index} className='gap-x-10'>
+                    {NavLink.title}
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
+          <div className='flex items-center gap-4'>
+            <div className='block md:hidden'>
+              <MobileNav />
+            </div>
+            <Button className='border-2 border-white p-5 rounded-full hover:bg-[#BFF265] hover:text-black cursor-pointer'>
+              Get in touch
+            </Button>
           </div>
         </div>
-        <div className='flex items-center gap-4'>
-          <div className='block md:hidden'>
-            <MobileNav />
-          </div>
-          <Button className='border-2 border-white p-5 rounded-full hover:bg-[#BFF265] hover:text-black cursor-pointer'>
-            Get in touch
-          </Button>
-        </div>
-      </div>
-    </header>
+      </header>
+      <hr className='text-[#BFF265]' />
+    </>
   )
 }
 
